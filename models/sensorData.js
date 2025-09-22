11f8d84f52accaccec5ed16c38ba6b961cfe718b
Ashwani pal <npal03563@gmail.com>
2025-09-22 11:33:38 +0530

    Add Day 3 backend code

--- models/sensorData.js
+++ models/sensorData.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 const sensorDataSchema = new mongoose.Schema({
-farmer: { type: mongoose.Schema.Types.ObjectId, ref: "farmer", required: true},
-soilMoisture: { type: Number, required: true},
-temprature: { type: Number, required: true},
-humidity: { type: Number, required: true}
-}, { timestamps: true});
-module.exports = mongoose.model("SensorData, sensorDataSchema");
+
+    temprature: { type: Number, required: true },
+    humiditiy: { type: Number, required: true },
+    soilMoisture: { type: Number, required: true },
+    createdAt : { type: Date, default: Date.now }
+
+
+
+
+});
+module.exports = mongoose.model("SensorData", sensorDataSchema );
